1beb57664600160ea3da0cebb9be391317936662
Miles <vegasfh07@gmail.com>
2026-05-25 03:30:20 -0300

    miles -updt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
     implementation(libs.activity.ktx)
     implementation(libs.appcompat)
+    implementation(libs.cardview)
     implementation(libs.constraintlayout)
     implementation(libs.material)
     testImplementation(libs.junit)
